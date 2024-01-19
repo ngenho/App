6c430ba9d2f91ab226a204be039a8812af82fc90
ngenho <157173125+ngenho@users.noreply.github.com>
2024-01-19 16:40:14 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+import streamlit as st, pandas as pd, numpy as np
+
+st.title('nates cite')
+st.header('Streamlit header')
+st.subheader('Sweet')
+
+st.sidebar.slider("x",0,10)
+
+st.text_input("whats your name")
